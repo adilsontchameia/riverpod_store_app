72c106bee40cd4b14beb9411b60d2080f19f183f
adilsontchameia <adilsontchameia@gmail.com>
2024-03-20 10:10:45 +0100

    feat: login and logout from provider

--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -12,9 +12,37 @@ class AuthNotifier extends StateNotifier<AuthState> {
   final AuthRepository authRepository;
   AuthNotifier({required this.authRepository}) : super(AuthState());
 
-  void login(String email, String password) async {}
-  void register(String email, String password, String fullName) async {}
-  void checkAuthStatus() async {}
+  Future<void> login(String email, String password) async {
+    await Future.delayed(const Duration(microseconds: 500));
+    try {
+      final user = await authRepository.login(email, password);
+      _setLoggedUser(user);
+    } on WrongCredentials {
+      logout('Wrong credentials');
+    } catch (e) {
+      logout('Exepetion not controlled');
+    }
+  }
+
+  Future<void> register(String email, String password, String fullName) async {}
+  Future<void> checkAuthStatus() async {}
+  Future<void> logout([String? errorMessage]) async {
+    //TODO: clean token
+    state = state.copyWith(
+      authStatus: AuthStatus.notAuthenticated,
+      user: null,
+      errorMessage: errorMessage,
+    );
+  }
+
+  void _setLoggedUser(User user) {
+    //TODO: needs to save token physically
+    state = state.copyWith(
+      user: user,
+      errorMessage: '',
+      authStatus: AuthStatus.authenticated,
+    );
+  }
 }
 
 enum AuthStatus { checking, authenticated, notAuthenticated }
