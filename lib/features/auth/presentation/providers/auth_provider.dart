6787079c732a99a223aedd64b1b31de5576d16f6
adilsontchameia <adilsontchameia@gmail.com>
2024-03-20 10:02:25 +0100

    feat: Auth Provider

---
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -0,0 +1,43 @@
+import '../../domain/domain.dart';
+import '../../infra/infra.dart';
+import 'providers.dart';
+
+final authProvider = StateNotifierProvider<AuthNotifier, AuthState>((ref) {
+  final authRepository = AuthRepositoryImpl();
+
+  return AuthNotifier(authRepository: authRepository);
+});
+
+class AuthNotifier extends StateNotifier<AuthState> {
+  final AuthRepository authRepository;
+  AuthNotifier({required this.authRepository}) : super(AuthState());
+
+  void login(String email, String password) async {}
+  void register(String email, String password, String fullName) async {}
+  void checkAuthStatus() async {}
+}
+
+enum AuthStatus { checking, authenticated, notAuthenticated }
+
+class AuthState {
+  final AuthStatus authStatus;
+  final User? user;
+  final String errorMessage;
+
+  AuthState({
+    this.authStatus = AuthStatus.checking,
+    this.user,
+    this.errorMessage = '',
+  });
+
+  AuthState copyWith({
+    AuthStatus? authStatus,
+    final User? user,
+    final String? errorMessage,
+  }) =>
+      AuthState(
+        authStatus: authStatus ?? this.authStatus,
+        user: user ?? this.user,
+        errorMessage: errorMessage ?? this.errorMessage,
+      );
+}
---
+++ lib/features/auth/presentation/providers/providers.dart
@@ -0,0 +1,3 @@
+export 'auth_provider.dart';
+export 'login_form_provider.dart';
+export 'package:flutter_riverpod/flutter_riverpod.dart';
