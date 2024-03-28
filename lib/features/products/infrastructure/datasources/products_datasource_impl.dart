a0a3ff1cb5c5e8a3b6cfa77392194fdc371116a1
adilsontchameia <adilsontchameia@gmail.com>
2024-03-28 11:01:13 +0100

    feat: feat: implementation of getProductsByPage

--- lib/features/products/infrastructure/datasources/products_datasource_impl.dart
+++ lib/features/products/infrastructure/datasources/products_datasource_impl.dart
@@ -1,6 +1,21 @@
+import 'package:dio/dio.dart';
+import '../../../../config/config.dart';
 import '../../domain/domain.dart';
 
 class ProductsDatasourceImpl extends ProductsDatasource {
+  late final Dio dio;
+  final String accesToken;
+
+  ProductsDatasourceImpl({
+    required this.accesToken,
+  }) : dio = Dio(
+          BaseOptions(
+            baseUrl: Environment.apiUrl,
+            headers: {
+              'Authorization': 'Bearer $accesToken',
+            },
+          ),
+        );
   @override
   Future<List<Product>> createUpdateProduct(Map<String, dynamic> productLike) {
     // TODO: implement createUpdateProduct
@@ -14,9 +29,15 @@ class ProductsDatasourceImpl extends ProductsDatasource {
   }
 
   @override
-  Future<List<Product>> getProductByPage({int limit = 10, int offset = 0}) {
-    // TODO: implement getProductByPage
-    throw UnimplementedError();
+  Future<List<Product>> getProductByPage(
+      {int limit = 10, int offset = 0}) async {
+    final response =
+        await dio.get<List>('api/products?limit=$limit&offset=$offset');
+    final List<Product> products = [];
+    for (final product in response.data ?? []) {
+      //products.add();
+    }
+    return products;
   }
 
   @override
