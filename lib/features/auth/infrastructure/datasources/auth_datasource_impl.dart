c8fec0b9947414a1f29649da3bd0a84bcb0cebf3
adilsontchameia <adilsontchameia@gmail.com>
2024-03-28 13:03:45 +0100

    feat: show not controlled error on screen

--- lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
+++ lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
@@ -4,66 +4,49 @@ import 'package:teslo_shop/features/auth/domain/domain.dart';
 import 'package:teslo_shop/features/auth/infrastructure/infrastructure.dart';
 
 class AuthDataSourceImpl extends AuthDataSource {
-
-  final dio = Dio(
-    BaseOptions(
-      baseUrl: Environment.apiUrl,
-    )
-  );
+  final dio = Dio(BaseOptions(
+    baseUrl: Environment.apiUrl,
+  ));
 
   @override
   Future<User> checkAuthStatus(String token) async {
-    
     try {
-      
       final response = await dio.get('/auth/check-status',
-        options: Options(
-          headers: {
-            'Authorization': 'Bearer $token'
-          }
-        )
-      );
+          options: Options(headers: {'Authorization': 'Bearer $token'}));
 
       final user = UserMapper.userJsonToEntity(response.data);
       return user;
-
-
-    } on DioError catch (e) {
-      if( e.response?.statusCode == 401 ){
-         throw CustomError('Token incorrecto');
+    } on DioException catch (e) {
+      if (e.response?.statusCode == 401) {
+        throw CustomError('Token incorrecto');
       }
       throw Exception();
     } catch (e) {
       throw Exception();
     }
-
   }
 
   @override
   Future<User> login(String email, String password) async {
-    
     try {
-      final response = await dio.post('/auth/login', data: {
-        'email': email,
-        'password': password
-      });
+      final response = await dio
+          .post('/auth/login', data: {'email': email, 'password': password});
 
       final user = UserMapper.userJsonToEntity(response.data);
       return user;
-      
-    } on DioError catch (e) {
-      if( e.response?.statusCode == 401 ){
-         throw CustomError(e.response?.data['message'] ?? 'Credenciales incorrectas' );
+    } on DioException catch (e) {
+      if (e.response?.statusCode == 401) {
+        throw CustomError(
+            e.response?.data['message'] ?? 'Credenciales incorrectas');
       }
-      if ( e.type == DioErrorType.connectionTimeout ){
-        throw CustomError('Revisar conexión a internet');
+      if (e.type == DioExceptionType.connectionTimeout ||
+          e.type == DioExceptionType.connectionError) {
+        throw CustomError('Please check the internet connection');
       }
-      throw Exception();
+      throw CustomError('Unknown Error: $e');
     } catch (e) {
-      throw Exception();
+      throw CustomError('Unknown Error: $e');
     }
-
-
   }
 
   @override
@@ -71,5 +54,4 @@ class AuthDataSourceImpl extends AuthDataSource {
     // TODO: implement register
     throw UnimplementedError();
   }
-  
 }
--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -4,72 +4,57 @@ import 'package:teslo_shop/features/auth/infrastructure/infrastructure.dart';
 import 'package:teslo_shop/features/shared/infrastructure/services/key_value_storage_service.dart';
 import 'package:teslo_shop/features/shared/infrastructure/services/key_value_storage_service_impl.dart';
 
-
-final authProvider = StateNotifierProvider<AuthNotifier,AuthState>((ref) {
-
+final authProvider = StateNotifierProvider<AuthNotifier, AuthState>((ref) {
   final authRepository = AuthRepositoryImpl();
   final keyValueStorageService = KeyValueStorageServiceImpl();
 
-
   return AuthNotifier(
-    authRepository: authRepository,
-    keyValueStorageService: keyValueStorageService
-  );
+      authRepository: authRepository,
+      keyValueStorageService: keyValueStorageService);
 });
 
-
-
 class AuthNotifier extends StateNotifier<AuthState> {
-
   final AuthRepository authRepository;
   final KeyValueStorageService keyValueStorageService;
 
   AuthNotifier({
     required this.authRepository,
     required this.keyValueStorageService,
-  }): super( AuthState() ) {
+  }) : super(AuthState()) {
     checkAuthStatus();
   }
-  
 
-  Future<void> loginUser( String email, String password ) async {
+  Future<void> loginUser(String email, String password) async {
     await Future.delayed(const Duration(milliseconds: 500));
 
     try {
       final user = await authRepository.login(email, password);
-      _setLoggedUser( user );
-
+      _setLoggedUser(user);
     } on CustomError catch (e) {
-      logout( e.message );
-    } catch (e){
-      logout( 'Error no controlado' );
+      logout(e.message);
+    } catch (e) {
+      logout('Error no controlado: $e');
     }
 
     // final user = await authRepository.login(email, password);
     // state =state.copyWith(user: user, authStatus: AuthStatus.authenticated)
-
   }
 
-  void registerUser( String email, String password ) async {
-    
-  }
+  void registerUser(String email, String password) async {}
 
   void checkAuthStatus() async {
     final token = await keyValueStorageService.getValue<String>('token');
-    if( token == null ) return logout();
+    if (token == null) return logout();
 
     try {
       final user = await authRepository.checkAuthStatus(token);
       _setLoggedUser(user);
-
     } catch (e) {
       logout();
     }
-
   }
 
-  void _setLoggedUser( User user ) async {
-    
+  void _setLoggedUser(User user) async {
     await keyValueStorageService.setKeyValue('token', user.token);
 
     state = state.copyWith(
@@ -79,46 +64,35 @@ class AuthNotifier extends StateNotifier<AuthState> {
     );
   }
 
-  Future<void> logout([ String? errorMessage ]) async {
-    
+  Future<void> logout([String? errorMessage]) async {
     await keyValueStorageService.removeKey('token');
 
     state = state.copyWith(
-      authStatus: AuthStatus.notAuthenticated,
-      user: null,
-      errorMessage: errorMessage
-    );
+        authStatus: AuthStatus.notAuthenticated,
+        user: null,
+        errorMessage: errorMessage);
   }
-
 }
-
-
 
 enum AuthStatus { checking, authenticated, notAuthenticated }
 
 class AuthState {
-
   final AuthStatus authStatus;
   final User? user;
   final String errorMessage;
 
-  AuthState({
-    this.authStatus = AuthStatus.checking, 
-    this.user, 
-    this.errorMessage = ''
-  });
+  AuthState(
+      {this.authStatus = AuthStatus.checking,
+      this.user,
+      this.errorMessage = ''});
 
   AuthState copyWith({
     AuthStatus? authStatus,
     User? user,
     String? errorMessage,
-  }) => AuthState(
-    authStatus: authStatus ?? this.authStatus,
-    user: user ?? this.user,
-    errorMessage: errorMessage ?? this.errorMessage
-  );
-
-
-
-
+  }) =>
+      AuthState(
+          authStatus: authStatus ?? this.authStatus,
+          user: user ?? this.user,
+          errorMessage: errorMessage ?? this.errorMessage);
 }
