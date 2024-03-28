7c7f15962ccf60e0bd796de996f96717a9614a51
adilsontchameia <adilsontchameia@gmail.com>
2024-03-28 12:18:43 +0100

    feat: product Riverpod - StateNotifierProvider - Notifier

--- lib/features/products/presentation/provider/products_provider.dart
+++ lib/features/products/presentation/provider/products_provider.dart
@@ -1,32 +1,73 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import '../../domain/domain.dart';
 
+//!1
 class ProductsState {
-  final bool isLatPage;
+  final bool isLastPage;
   final int limit;
   final int offset;
   final bool isLoading;
   final List<Product> products;
 
   ProductsState({
-    this.isLatPage = false,
+    this.isLastPage = false,
     this.limit = 10,
     this.offset = 0,
     this.isLoading = false,
     this.products = const [],
   });
 
-  ProductsState copyWith(
-    bool? isLatPage,
+  ProductsState copyWith({
+    bool? isLastPage,
     int? limit,
     int? offset,
     bool? isLoading,
     List<Product>? products,
-  ) =>
+  }) =>
       ProductsState(
-        isLatPage: isLatPage ?? this.isLatPage,
+        isLastPage: isLastPage ?? this.isLastPage,
         limit: limit ?? this.limit,
         offset: offset ?? this.offset,
         isLoading: isLoading ?? this.isLoading,
         products: products ?? this.products,
       );
+}
+
+//!2
+class ProductsNotifier extends StateNotifier<ProductsState> {
+  final ProductRepository productRepository;
+  ProductsNotifier({required this.productRepository}) : super(ProductsState()) {
+    loadNextPage();
+  }
+
+  Future loadNextPage() async {
+    //To avoid multiple requests - infinite scroll
+    if (state.isLoading || state.isLastPage) return;
+
+    state = state.copyWith(isLoading: true);
+
+    final products = await productRepository.getProductByPage(
+      limit: state.limit,
+      offset: state.offset,
+    );
+
+    if (products.isEmpty) {
+      state = state.copyWith(
+        isLoading: false,
+        isLastPage: true,
+      );
+      return;
+    }
+
+    state = state.copyWith(
+      isLoading: false,
+      isLastPage: false,
+      offset: state.offset + 10,
+      products: [
+        ...state.products,
+        ...products,
+      ],
+    );
+  }
 }
