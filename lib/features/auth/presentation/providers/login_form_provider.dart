84db4ee41c414656349f301d8f0cb8aa04aa37bf
adilsontchameia <adilsontchameia@gmail.com>
2024-03-28 10:20:22 +0100

    feat: onFormSubmited

--- lib/features/auth/presentation/providers/login_form_provider.dart
+++ lib/features/auth/presentation/providers/login_form_provider.dart
@@ -4,87 +4,72 @@ import 'package:teslo_shop/features/auth/presentation/providers/auth_provider.da
 import 'package:teslo_shop/features/shared/shared.dart';
 
 //! 3 - StateNotifierProvider - consume afuera
-final loginFormProvider = StateNotifierProvider.autoDispose<LoginFormNotifier,LoginFormState>((ref) {
-
+final loginFormProvider =
+    StateNotifierProvider.autoDispose<LoginFormNotifier, LoginFormState>((ref) {
   final loginUserCallback = ref.watch(authProvider.notifier).loginUser;
 
-
-  return LoginFormNotifier(
-    loginUserCallback:loginUserCallback
-  );
+  return LoginFormNotifier(loginUserCallback: loginUserCallback);
 });
-
 
 //! 2 - Como implementamos un notifier
 class LoginFormNotifier extends StateNotifier<LoginFormState> {
-
   final Function(String, String) loginUserCallback;
 
   LoginFormNotifier({
     required this.loginUserCallback,
-  }): super( LoginFormState() );
-  
-  onEmailChange( String value ) {
+  }) : super(LoginFormState());
+
+  void onEmailChange(String value) {
     final newEmail = Email.dirty(value);
     state = state.copyWith(
-      email: newEmail,
-      isValid: Formz.validate([ newEmail, state.password ])
-    );
+        email: newEmail, isValid: Formz.validate([newEmail, state.password]));
   }
 
-  onPasswordChanged( String value ) {
+  void onPasswordChanged(String value) {
     final newPassword = Password.dirty(value);
     state = state.copyWith(
-      password: newPassword,
-      isValid: Formz.validate([ newPassword, state.email ])
-    );
+        password: newPassword,
+        isValid: Formz.validate([newPassword, state.email]));
   }
 
   onFormSubmit() async {
     _touchEveryField();
 
-    if ( !state.isValid ) return;
+    if (!state.isValid) return;
 
     state = state.copyWith(isPosting: true);
 
-    await loginUserCallback( state.email.value, state.password.value );
+    await loginUserCallback(state.email.value, state.password.value);
 
     state = state.copyWith(isPosting: false);
   }
 
   _touchEveryField() {
-
-    final email    = Email.dirty(state.email.value);
+    final email = Email.dirty(state.email.value);
     final password = Password.dirty(state.password.value);
 
     state = state.copyWith(
-      isFormPosted: true,
-      email: email,
-      password: password,
-      isValid: Formz.validate([ email, password ])
-    );
-
+        isFormPosted: true,
+        email: email,
+        password: password,
+        isValid: Formz.validate([email, password]));
   }
-
 }
-
 
 //! 1 - State del provider
 class LoginFormState {
-
   final bool isPosting;
   final bool isFormPosted;
   final bool isValid;
   final Email email;
   final Password password;
 
-  LoginFormState({
-    this.isPosting = false,
-    this.isFormPosted = false,
-    this.isValid = false,
-    this.email = const Email.pure(),
-    this.password = const Password.pure()
-  });
+  LoginFormState(
+      {this.isPosting = false,
+      this.isFormPosted = false,
+      this.isValid = false,
+      this.email = const Email.pure(),
+      this.password = const Password.pure()});
 
   LoginFormState copyWith({
     bool? isPosting,
@@ -92,13 +77,14 @@ class LoginFormState {
     bool? isValid,
     Email? email,
     Password? password,
-  }) => LoginFormState(
-    isPosting: isPosting ?? this.isPosting,
-    isFormPosted: isFormPosted ?? this.isFormPosted,
-    isValid: isValid ?? this.isValid,
-    email: email ?? this.email,
-    password: password ?? this.password,
-  );
+  }) =>
+      LoginFormState(
+        isPosting: isPosting ?? this.isPosting,
+        isFormPosted: isFormPosted ?? this.isFormPosted,
+        isValid: isValid ?? this.isValid,
+        email: email ?? this.email,
+        password: password ?? this.password,
+      );
 
   @override
   String toString() {
--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -5,74 +5,66 @@ import 'package:teslo_shop/features/auth/presentation/providers/auth_provider.da
 import 'package:teslo_shop/features/auth/presentation/providers/providers.dart';
 import 'package:teslo_shop/features/shared/shared.dart';
 
-
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     final size = MediaQuery.of(context).size;
     final scaffoldBackgroundColor = Theme.of(context).scaffoldBackgroundColor;
 
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
       child: Scaffold(
-        body: GeometricalBackground( 
-          child: SingleChildScrollView(
-            physics: const ClampingScrollPhysics(),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox( height: 80 ),
-                // Icon Banner
-                const Icon( 
-                  Icons.production_quantity_limits_rounded, 
-                  color: Colors.white,
-                  size: 100,
-                ),
-                const SizedBox( height: 80 ),
-    
-                Container(
-                  height: size.height - 260, // 80 los dos sizebox y 100 el ícono
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: scaffoldBackgroundColor,
-                    borderRadius: const BorderRadius.only(topLeft: Radius.circular(100)),
-                  ),
-                  child: const _LoginForm(),
-                )
-              ],
+          body: GeometricalBackground(
+              child: SingleChildScrollView(
+        physics: const ClampingScrollPhysics(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 80),
+            // Icon Banner
+            const Icon(
+              Icons.production_quantity_limits_rounded,
+              color: Colors.white,
+              size: 100,
             ),
-          )
-        )
-      ),
+            const SizedBox(height: 80),
+
+            Container(
+              height: size.height - 260, // 80 los dos sizebox y 100 el ícono
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: scaffoldBackgroundColor,
+                borderRadius:
+                    const BorderRadius.only(topLeft: Radius.circular(100)),
+              ),
+              child: const _LoginForm(),
+            )
+          ],
+        ),
+      ))),
     );
   }
 }
 
 class _LoginForm extends ConsumerWidget {
-
   const _LoginForm();
 
-  void showSnackbar( BuildContext context, String message ) {
+  void showSnackbar(BuildContext context, String message) {
     ScaffoldMessenger.of(context).hideCurrentSnackBar();
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(message))
-    );
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
   }
-
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     final loginForm = ref.watch(loginFormProvider);
 
     ref.listen(authProvider, (previous, next) {
-      if ( next.errorMessage.isEmpty ) return;
-      showSnackbar( context, next.errorMessage );
+      if (next.errorMessage.isEmpty) return;
+      showSnackbar(context, next.errorMessage);
     });
-
 
     final textStyles = Theme.of(context).textTheme;
 
@@ -80,57 +72,47 @@ class _LoginForm extends ConsumerWidget {
       padding: const EdgeInsets.symmetric(horizontal: 50),
       child: Column(
         children: [
-          const SizedBox( height: 50 ),
-          Text('Login', style: textStyles.titleLarge ),
-          const SizedBox( height: 90 ),
-
+          const SizedBox(height: 50),
+          Text('Login', style: textStyles.titleLarge),
+          const SizedBox(height: 90),
           CustomTextFormField(
             label: 'Correo',
             keyboardType: TextInputType.emailAddress,
             onChanged: ref.read(loginFormProvider.notifier).onEmailChange,
-            errorMessage: loginForm.isFormPosted ?
-               loginForm.email.errorMessage 
-               : null,
+            errorMessage:
+                loginForm.isFormPosted ? loginForm.email.errorMessage : null,
           ),
-          const SizedBox( height: 30 ),
-
+          const SizedBox(height: 30),
           CustomTextFormField(
             label: 'Contraseña',
             obscureText: true,
             onChanged: ref.read(loginFormProvider.notifier).onPasswordChanged,
-            errorMessage: loginForm.isFormPosted ?
-               loginForm.password.errorMessage 
-               : null,
+            onFieldSubmitted: (_) =>
+                ref.read(loginFormProvider.notifier).onFormSubmit(),
+            errorMessage:
+                loginForm.isFormPosted ? loginForm.password.errorMessage : null,
           ),
-    
-          const SizedBox( height: 30 ),
-
+          const SizedBox(height: 30),
           SizedBox(
-            width: double.infinity,
-            height: 60,
-            child: CustomFilledButton(
-              text: 'Ingresar',
-              buttonColor: Colors.black,
-              onPressed: loginForm.isPosting
-                ? null 
-                : ref.read(loginFormProvider.notifier).onFormSubmit
-            )
-          ),
-
-          const Spacer( flex: 2 ),
-
+              width: double.infinity,
+              height: 60,
+              child: CustomFilledButton(
+                  text: 'Ingresar',
+                  buttonColor: Colors.black,
+                  onPressed: loginForm.isPosting
+                      ? null
+                      : ref.read(loginFormProvider.notifier).onFormSubmit)),
+          const Spacer(flex: 2),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const Text('¿No tienes cuenta?'),
               TextButton(
-                onPressed: ()=> context.push('/register'), 
-                child: const Text('Crea una aquí')
-              )
+                  onPressed: () => context.push('/register'),
+                  child: const Text('Crea una aquí'))
             ],
           ),
-
-          const Spacer( flex: 1),
+          const Spacer(flex: 1),
         ],
       ),
     );
