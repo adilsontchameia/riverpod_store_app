6e14698ca9f17fb47939bd5871863169d6e819ea
adilsontchameia <adilsontchameia@gmail.com>
2024-03-28 11:48:33 +0100

    feat: Riverpod - Product Repository Provider

---
+++ lib/features/products/presentation/provider/products_repository_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../auth/presentation/providers/auth_provider.dart';
+import '../../domain/domain.dart';
+import '../../infrastructure/infrastructure.dart';
+
+final productsRepositoryProvider = Provider<ProductRepository>((ref) {
+  final accessToken = ref.watch(authProvider).user?.token ?? '';
+
+  final productsRepository = ProductsRepositoryImpl(
+    ProductsDatasourceImpl(accesToken: accessToken),
+  );
+
+  return productsRepository;
+});
