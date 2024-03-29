68b07bb59ef5f04ffd029d55bfd88fe6d1e059f5
adilsontchameia <adilsontchameia@gmail.com>
2024-03-29 11:32:42 +0100

    feat: show product information

---
+++ lib/features/products/presentation/widgets/fullscreen_loader.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FullScreenLoader extends StatelessWidget {
+  const FullScreenLoader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.expand(
+      child: Center(
+        child: CircularProgressIndicator.adaptive(),
+      ),
+    );
+  }
+}
--- lib/features/products/presentation/widgets/widgets.dart
+++ lib/features/products/presentation/widgets/widgets.dart
@@ -1,1 +1,2 @@
 export 'product_card.dart';
+export 'fullscreen_loader.dart';
