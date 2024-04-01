4497f049a8fff47f8066c3e087e777077b63267a
adilsontchameia <adilsontchameia@gmail.com>
2024-04-01 10:47:19 +0100

    feat: Product Form Provider - State

---
+++ lib/features/products/presentation/provider/form/product_form_provider.dart
@@ -0,0 +1,63 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import '../../../../shared/infrastructure/inputs/inputs.dart';
+
+class ProductFormState {
+  final bool isFormValid;
+  final String? id;
+  final Title? title;
+  final Slug? slug;
+
+  final Price? price;
+
+  final List<String> sizes;
+
+  final String? gender;
+
+  final Stock? inStock;
+
+  final String? description;
+
+  final String tags;
+  final List<String> images;
+
+  ProductFormState({
+    this.isFormValid = false,
+    this.id,
+    this.title = const Title.dirty(''),
+    this.slug = const Slug.dirty(''),
+    this.price = const Price.dirty(0),
+    this.sizes = const [],
+    this.gender = 'men',
+    this.inStock = const Stock.dirty(0),
+    this.description = '',
+    this.tags = '',
+    this.images = const [],
+  });
+
+  ProductFormState copyWith({
+    bool? isFormValid,
+    String? id,
+    Title? title,
+    Slug? slug,
+    Price? price,
+    List<String>? sizes,
+    String? gender,
+    Stock? inStock,
+    String? description,
+    String? tags,
+    List<String>? images,
+  }) =>
+      ProductFormState(
+        isFormValid: isFormValid ?? this.isFormValid,
+        id: id ?? this.id,
+        title: title ?? this.title,
+        slug: slug ?? this.slug,
+        price: price ?? this.price,
+        sizes: sizes ?? this.sizes,
+        gender: gender ?? this.gender,
+        inStock: inStock ?? this.inStock,
+        description: description ?? this.description,
+        tags: tags ?? this.tags,
+        images: images ?? this.images,
+      );
+}
